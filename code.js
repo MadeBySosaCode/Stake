80625967f621173c8ff0b92db8ce1f3f40e81439
SosaCode. <113245064+MadeBySosaCode@users.noreply.github.com>
2024-12-16 18:35:53 -0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,184 @@
+const SosaCodeF = SosaCodeM,
+	SosaCodeq = SosaCodeM,
+	SosaCodev = SosaCodeM,
+	SosaCodel = SosaCodeM,
+	SosaCodeE = SosaCodeM,
+	SosaCodeu = SosaCodeM,
+	SosaCodet = SosaCodeM,
+	SosaCodej = SosaCodeM,
+	SosaCodef = SosaCodeM,
+	SosaCodeO = SosaCodeM,
+	SosaCodeC = SosaCodeM,
+	SosaCodep = SosaCodeM,
+	SosaCodeo = SosaCodeM,
+	SosaCodeD = SosaCodeM,
+	SosaCodec = SosaCodeM,
+	SosaCodeh = SosaCodeM,
+	SosaCoder = SosaCodeM,
+	SosaCodeP = SosaCodeM,
+	SosaCodex = SosaCodeM,
+	SosaCodea = SosaCodeM,
+	SosaCoded = SosaCodeM,
+	SosaCodeY = SosaCodeM,
+	SosaCodeZ = SosaCodeM,
+	SosaCodeQ = SosaCodeM,
+	SosaCodey = SosaCodeM,
+	SosaCodeB = SosaCodeM;
+(function(K, M) {
+	const SosaCodeU = {
+			K: 0xc5,
+			M: 0xd2,
+			X: 0xc3,
+			A: 0xcb,
+			I: 0xc9,
+			m: 0xc4,
+			L: 0xbb,
+			N: 0xbf,
+			i: 0xba,
+			J: 0xd4
+		},
+		n = SosaCodeM,
+		e = SosaCodeM,
+		z = SosaCodeM,
+		g = SosaCodeM,
+		V = SosaCodeM,
+		S = SosaCodeM,
+		w = SosaCodeM,
+		b = SosaCodeM,
+		W = SosaCodeM,
+		H = SosaCodeM,
+		X = K();
+	while (!![]) {
+		try {
+			const A = -parseInt(n(
+						SosaCodeU.K
+						)) / 0x1 +
+				parseInt(e(SosaCodeU
+					.M)) / 0x2 * (-
+					parseInt(n(
+						SosaCodeU
+						.X)) / 0x3
+					) + parseInt(n(
+						SosaCodeU.A
+						)) / 0x4 + -
+				parseInt(V(SosaCodeU
+					.I)) / 0x5 + -
+				parseInt(e(SosaCodeU
+					.m)) / 0x6 * (
+					parseInt(S(
+						SosaCodeU
+						.L)) / 0x7
+					) + parseInt(S(
+						SosaCodeU.N
+						)) / 0x8 * (
+					parseInt(w(
+						SosaCodeU
+						.i)) / 0x9
+					) + parseInt(z(
+						SosaCodeU.J
+						)) / 0xa;
+			if (A === M)
+				break;
+			else
+				X['push'](X['shift']
+					());
+		} catch (I) {
+			X['push'](X['shift']());
+		}
+	}
+}(SosaCodeK, 0xba990));
+
+function SosaCodeK() {
+	const R = [
+		'6300280gQsRjS',
+		'from',
+		'2467968IjzJBJ',
+		'fill',
+		'Invalid\x20input.\x20Setting\x20default\x20to\x203.',
+		'join',
+		'Version:\x20Unstable\x20might\x20not\x20work\x20anymore.\x0a',
+		'You\x27re\x20cID\x27s:\x20',
+		'log',
+		'166IuGLsH',
+		'random',
+		'39863670AWeRrF',
+		'map',
+		'1545084IVnUng',
+		'1503523gvCiyb',
+		'Enter\x20the\x20number\x20of\x20Gem\x27s\x20(min\x201,\x20max\x2024):\x20',
+		'floor',
+		'has',
+		'32srWvzC',
+		'size',
+		'Created\x20by:\x20SosaCode.\x0a',
+		'Games:\x20Mines.\x0a\x0a',
+		'39723ZTFAwq',
+		'24fCNIUz',
+		'1307544XULTzz',
+		'Enter\x20you\x27re\x20cID\x27s:\x20',
+		'No\x20cID\x27s\x20provided.',
+		'add'
+	];
+	SosaCodeK = function() {
+		return R;
+	};
+	return SosaCodeK();
+}
+const SosaCodeX = 0x5,
+	SosaCodeA = 0x5;
+console[SosaCodeF(0xd1)](SosaCodeF(
+		0xc1)), console[SosaCodeF(0xd1)]
+	(SosaCodeF(0xcf)), console[
+		SosaCodeq(0xd1)](SosaCodel(
+		0xc2));
+const SosaCodeI = Array[SosaCodev(0xca)]
+	({
+		'length': SosaCodeX
+	}, () => Array(SosaCodeA)[
+		SosaCodeq(0xcc)]('X'));
+let SosaCodem = parseInt(prompt(
+	SosaCodej(0xbc)), 0xa);
+(isNaN(SosaCodem) || SosaCodem < 0x1 ||
+	SosaCodem > 0x18) && (console[
+	SosaCodej(0xd1)](SosaCodel(
+	0xcd)), SosaCodem = 0x3);
+const SosaCodeL = new Set();
+
+function SosaCodeM(K, M) {
+	const X = SosaCodeK();
+	return SosaCodeM = function(A, I) {
+		A = A - 0xba;
+		let m = X[A];
+		return m;
+	}, SosaCodeM(K, M);
+}
+while (SosaCodeL[SosaCodeO(0xc0)] <
+	SosaCodem) {
+	const SosaCodeJ = Math[SosaCodeo(
+			0xbd)](Math[SosaCodeO(0xd3)]
+			() * (SosaCodeX * SosaCodeA)
+			),
+		SosaCodeG = Math[SosaCodeo(
+			0xbd)](SosaCodeJ /
+			SosaCodeA),
+		SosaCodes = SosaCodeJ %
+		SosaCodeA,
+		SosaCodek = SosaCodeG + ',' +
+		SosaCodes;
+	!SosaCodeL[SosaCodeO(0xbe)](
+		SosaCodek) && (SosaCodeI[
+			SosaCodeG][SosaCodes] =
+		'O', SosaCodeL[SosaCodet(
+			0xc8)](SosaCodek));
+}
+const SosaCodeN = SosaCodeI[SosaCodeP(
+		0xd5)](K => K[SosaCodep(0xce)](
+		'\x20'))[SosaCodeD(0xce)](
+		'\x0a'),
+	SosaCodei = prompt(SosaCodea(0xc6));
+SosaCodei ? (console[SosaCodep(0xd1)](
+		SosaCodeu(0xd0), SosaCodei),
+	console[SosaCodeQ(0xd1)](
+		SosaCodeN)) : console[
+	SosaCodeC(0xd1)](SosaCodeO(
+	0xc7));
